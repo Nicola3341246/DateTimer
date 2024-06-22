69fdd4a2082b236e58f5d0699232dbe3383e2a90
Egloff Nicola BBB <89132258+Nicola3341246@users.noreply.github.com>
2024-06-22 18:30:30 +0200

    Added placeholder routing

--- timer-app/src/app/app.routes.ts
+++ timer-app/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { DateTimerComponent } from './dateTimer/dateTimer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'dateTimer', component: DateTimerComponent},
+];
---
+++ timer-app/src/app/dateTimer/dateTimer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'date-timer',
+    standalone: true,
+    templateUrl: './dateTimer.component.html',
+})
+
+export class DateTimerComponent {
+}
