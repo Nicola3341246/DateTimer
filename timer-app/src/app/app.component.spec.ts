6b63d4bafe2f351ad8bc22069b08d96214af2f8f
Egloff Nicola BBB <89132258+Nicola3341246@users.noreply.github.com>
2024-06-23 18:38:51 +0200

    Fixed small bug

--- timer-app/src/app/app.component.spec.ts
+++ timer-app/src/app/app.component.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { DateTimerComponent } from './dateTimerForm/dateTimerForm.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
--- timer-app/src/app/app.component.ts
+++ timer-app/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { DateTimerComponent } from './dateTimerForm/dateTimerForm.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, DateTimerComponent],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+  title = 'The App of the Time';
 }
