664fb371d6db937712c0e4709e4789156e350b4f
Egloff Nicola BBB <89132258+Nicola3341246@users.noreply.github.com>
2024-06-23 17:45:37 +0200

    Added Inputforms and temp styling

--- timer-app/src/app/dateTimer/dateTimer.component.ts
+++ timer-app/src/app/dateTimer/dateTimer.component.ts
@@ -1,10 +1,16 @@
 import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { get } from "http";
 
 @Component({
     selector: 'date-timer',
     standalone: true,
     templateUrl: './dateTimer.component.html',
+    styleUrl: './dateTimer.component.css',
+    imports: [FormsModule],
 })
 
 export class DateTimerComponent {
+    dateInputValue: string = new Date().toISOString().substring(0,10);
+    timeInputValue: string = '';
 }
